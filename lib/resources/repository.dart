83ad36e2418580eb638c0d10ecedf4c200ff6e50
Ernesto Chira Torres <ernesto@iMac-de-Ernesto.local>
2020-10-04 14:34:43 -0500

    login con bloc

--- lib/resources/repository.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:service_now/models/user.dart';
-// import 'package:service_now/resources/user_api_provider.dart';
-
-// class Repository {
-//   final userApiProvider = UserApiProvider();
-
-//   Future<LoginResponse> login(String email, String password) => userApiProvider.login(email, password);
-// }
